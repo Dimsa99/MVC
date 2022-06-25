7d62d21b36b00d65a131dfe499617302e63c1e75
Dimsa99 <dimsajosifovski@gmail.com>
2022-06-25 17:24:38 +0200

    Initial commit

---
+++ Class02/WebApplication1/WebApplication1/Controllers/PizzaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Database;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = PizzaDatabase.PIZZAS;
+            return View(pizzas);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Pizza pizza = PizzaDatabase.PIZZAS.FirstOrDefault(x => x.Id == id);
+            if( pizza is null)
+            {
+                return RedirectToAction("Error");
+            }
+            return View(pizza);
+        }
+    }
+}
---
+++ Class02/WebApplication1/WebApplication1/Database/PizzaDatabase.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Database
+{
+    public class PizzaDatabase
+    {
+        public static List<Pizza> PIZZAS = new List<Pizza>()
+        {
+            new Pizza() { Id = 1, Name = "Margareta", Price = 10m, IsOnPromotion = false},
+            new Pizza() { Id = 2, Name = "Capri", Price = 11m, IsOnPromotion = false},
+            new Pizza() { Id = 3, Name = "Quatro", Price = 8.99m, IsOnPromotion = true}
+        };
+    }
+}
